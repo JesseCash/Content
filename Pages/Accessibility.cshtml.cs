24bce22911951fb2a9fc730b0ade50a48fc284cf
JesseCash <151931330+JesseCash@users.noreply.github.com>
2025-04-02 08:47:43 +0100

    Add files via upload

---
+++ Pages/Accessibility.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rolsa_Technologies.Pages
+{
+    public class AccessibilityModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/EV_Locator.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Rolsa_Technologies.Pages
+{
+    public class EV_LocatorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
